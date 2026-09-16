fa6588ccdde0e3318e75b8fad9a103326a4fa812
Sergio PC-HOME <it59com@mail.ru>
2026-09-16 10:20:34 +0500

    Expo

--- lib/core/config/app_mock_config.dart
+++
@@ -1,8 +0,0 @@
-class AppMockConfig {
-  const AppMockConfig._();
-
-  static const bool enabled = bool.fromEnvironment(
-    'PASSENGER_USE_MOCKS',
-    defaultValue: false,
-  );
-}
--- lib/core/config/firebase_config.dart
+++
@@ -1,8 +0,0 @@
-class FirebaseConfig {
-  const FirebaseConfig._();
-
-  static const bool pushEnabled = bool.fromEnvironment(
-    'PASSENGER_PUSH_ENABLED',
-    defaultValue: true,
-  );
-}
